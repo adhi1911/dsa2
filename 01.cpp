e7c46b8f3518ad3ac147b0f52c534d820df8b216
Aaradhya Kulkarni <93368636+adhi1911@users.noreply.github.com>
2024-05-01 16:09:26 +0530

    Update 01.cpp

--- 01.cpp
+++ 01.cpp
@@ -1,154 +1,100 @@
-/*****************************************************************************
-Group A1 (Hashing)
-Consider telephone book database of N clients. Make use of a hash table implementation to quickly look up client‘s telephone number. Make use of two collision handling techniques and compare them using number of comparisons required to find a
-set of telephone numbers
-*****************************************************************************/
+#include<iostream>
+#include<cstdlib>
 using namespace std;
 
-#include<iostream>
 
-class HashTable
-{
-    public:
-    int index;
-    long int mobile;
+class Hashing{
+
+public:
+	long int Hash_Table[10];
+
+Hashing(){
+for(int i =0;i<10;i++) Hash_Table[i]=-1;
+}
+
+long int read(){
+	long int key;
+	cout<<"\nEntre Mobile Number(Key): ";
+	cin>>key;
+	return key;	
+}
+
+void linear_probe(long int key){
+	int index = key%10;
+
+	if(Hash_Table[index]==-1){
+		Hash_Table[index]=key;
+	}
+
+	else{
+		while(Hash_Table[index]!= -1){
+		index= (index +1)%10;
+		}
+		Hash_Table[index]=key;
+	}
+}
+
+void quadratic_probe(long int key){
+	int index = key%10;
+	int i =1;
+
+	if(Hash_Table[index]==-1){
+		Hash_Table[index]=key;
+	}
+	else{ 
+		while(Hash_Table[index]!= -1){
+		index= (key + (i*i))%10;
+		cout<<"**"<<index;
+		i++;
+		}
+		Hash_Table[index]=key;
+	}
+
+}
+void display(){
+	for(int i=0;i<10;i++){
+		cout<<Hash_Table[i]<<endl;
+	}
+}
+
+
 };
 
-class Hashing
-{
-    HashTable h[10];
-    public:
-    Hashing()
-    {
-        for(int i=0;i<10;i++)
-        {
-            h[i].index=i;
-            h[i].mobile=-1;
-        }
-    }
-    
-    void display();
-    void insert(int);
-    void search();
-    int LinearProbing(int);
-    int QuadraticProbing(int);
-    
-    
-};
-
-int Hashing::QuadraticProbing(int collision_position)
-{
-    
-}
-
-int Hashing::LinearProbing(int collision_position)
-{
-    for(int i=collision_position;i<10;i++)
-    {
-        if(h[i].mobile==-1)
-        return i;
-        
-        if(i==9)
-            i = -1;
-    }
-}
 
 
-void Hashing::display()
-{
-    for(int i=0;i<10;i++)
-    {
-        cout<<h[i].index;
-        cout<<"  "<<h[i].mobile;
-        cout<<endl;
-    }
-    
-}
+int main(){
 
-void Hashing::insert(int probchoice)
-{   
-    long int key;
-    int position;
-    cout<<"\nEnter mobile number to insert in to hash table : ";
-    cin>>key;
-    position = key % 10;
-    
-    cout<<"\nPosition = "<<position;
-    
-    if(h[position].mobile==-1)
-    {
-        h[position].mobile=key;
-    }
-    else if(probchoice ==1)// Linear Probing collision.
-    {
-        int temp_position;
-        temp_position = LinearProbing(position);
-        h[temp_position].mobile=key;
-    }
-    else if(probchoice ==2)// Quadratic Probing for collision.
-    {
-        
-    }
-    
-}
+Hashing hash;
 
-void Hashing::search()
-{   
-    long int key;
-    int position;
-    cout<<"\nEnter mobile number to search in the hash table : ";
-    cin>>key;
-    position = key % 10;
-    
-    if(h[position].mobile==key)
-    {
-        cout<<"\nGiven mobile number is found in the hash table ";
-    }
-    else
-    {
-        cout<<"\nGiven mobile number is NOT found in the hash table ";
-    }
-    
-}
-
-
-int main()
-{
-    Hashing H;
-    int ch;
-    
-    do
-    {
-        cout<<"\n Menu";
-        cout<<"\n 1. insert";
-        cout<<"\n 2.display";
-        cout<<"\n 3.search";
-
-        cout<<"\n 5. exit";
-        cout<<"\n Enter your choice : ";
-        cin>>ch;
-        switch(ch)
-        {
-            case 1 : //insert
-                int probchoice;
-                cout<<"\nEnter \n1 for LinearProbing and \n2 for Quadratic probing";
-                cin>>probchoice;
-                H.insert(probchoice);
-            break;
-            case 2 : //display
-                H.display();
-            break;
-            
-            case 3 : //display
-                H.search();
-            break;
-            
-            default:
-                cout<<"\nWrong choice :";
-            break;
-        }
-        
-    }while(ch != 5);
-    H.display();
-    
+	int ch;
+	int n;
+	while(true){
+	
+	cout<<"\n 0.Enter Number of keys \n 1. Display List \n 2. Linear Probe \n 3. Quadratic Probe \n 59. Flush Hash Table\n 60. Exit \n\n\n Enter choice: ";
+	cin>>ch;
+	switch(ch){
+		case 0: cin>>n;
+		        break;
+		case 1: hash.display();
+			  break;
+		case 2: for(int j =0;j<n;j++){
+			  hash.linear_probe(hash.read());
+			  }
+			  break;
+			  
+		case 3: for(int j =0;j<n;j++){
+		        hash.quadratic_probe(hash.read());
+			  }
+			  break;
+		case 59: for(int k=0;k<10;k++){
+				hash.Hash_Table[k]=-1;
+				}
+		case 60: exit(0);
+				break;
+		default: cout<<"Invalid input";
+			   break;
+		}
+	}
+	
+return 0;
 }
