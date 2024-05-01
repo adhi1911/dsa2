cde8e1ebe70f2e83f8d6a2f0081f6e7b46a7fe1b
Aaradhya Kulkarni <93368636+adhi1911@users.noreply.github.com>
2024-05-01 16:11:39 +0530

    Update 02.cpp

--- 02.cpp
+++ 02.cpp
@@ -1,290 +1,120 @@
-/*
-Assignment = 02
-
-Title:
-To create ADT that implement the ""set"" concept.
-a. Add (new Element) -Place a value into the set , b. Remove (element) Remove the value
-c. Contains (element) Return true if element is in collection, d. Size () Return number
-of values in collection Iterator () Return an iterator used to loop over collection,
-e. Intersection of two sets ,
-f. Union of two sets,
-g. Difference between two sets, h. Subset*/
-
-#include <iostream>
-#include <set>
+#include<iostream>
 using namespace std;
 
-class Set
-{
-private:
-    set<int> elements;
+class Set{
 
-public:
-    void add(int element)
-    {
-        elements.insert(element);
-    }
-
-    void remove(int element)
-    {
-        elements.erase(element);
-    }
-
-    bool contains(int element) const
-    {
-        return elements.count(element) > 0;
-    }
-
-    int size()
-    {
-        return elements.size();
-    }
-
-    void print()
-    {
-        for (int element : elements)
-        {
-            cout << element << " ";
-        }
-        cout << endl;
-    }
-
-    Set intersection(const Set &other)
-    {
-        Set result;
-        for (int element : elements)
-        {
-            if (other.contains(element))
-            {
-                result.add(element);
-            }
-        }
-        return result;
-    }
-
-    Set unionSet(const Set &other)
-    {
-        Set result;
-        for (int element : elements)
-        {
-            result.add(element);
-        }
-        for (int element : other.elements)
-        {
-            result.add(element);
-        }
-        return result;
-    }
-
-    Set difference(const Set &other)
-    {
-        Set result;
-        for (int element : elements)
-        {
-            if (!other.contains(element))
-            {
-                result.add(element);
-            }
-        }
-        return result;
-    }
-
-    bool subset(const Set &other)
-    {
-        for (int element : elements)
-        {
-            if (!other.contains(element))
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    class Iterator
-    {
-    private:
-        set<int>::iterator it;
-
-    public:
-        Iterator(set<int>::iterator iterator) : it(iterator) {}
-
-        int operator*()
-        {
-            return *it;
-        }
-
-        Iterator operator++()
-        {
-            ++it;
-            return *this;
-        }
-
-        bool operator!=(const Iterator &other)
-        {
-            return it != other.it;
-        }
-    };
-
-    Iterator begin()
-    {
-        return Iterator(elements.begin());
-    }
-
-    Iterator end()
-    {
-        return Iterator(elements.end());
-    }
+	int a[20];  //n is size
+	public:
+	int n;
+		Set(){ n=0;} //default constructor to initialise n
+		void Create();
+		void Display();
+		int Search(int x);
+		void Union(Set A, Set B);
+		void Intersect(Set A,Set B);
+		
 };
 
-int main()
-{
-    Set set;
-    int choice, element;
-    while (true)
-    {
-        cout << "Menu:\n";
-        cout << "1. Add\n";
-        cout << "2. Remove\n";
-        cout << "3. Contains\n";
-        cout << "4. Size\n";
-        cout << "5. Print\n";
-        cout << "6. Intersection\n";
-        cout << "7. Union\n";
-        cout << "8. Difference\n";
-        cout << "9. Subset\n";
-        cout << "0. Exit\n";
-        cout << "Enter your choice: ";
-        cin >> choice;
-
-        switch (choice)
-        {
-        case 1:
-            cout << "Enter element to add: ";
-            cin >> element;
-            set.add(element);
-            break;
-        case 2:
-            cout << "Enter element to remove: ";
-            cin >> element;
-            set.remove(element);
-            break;
-        case 3:
-            cout << "Enter element to check: ";
-            cin >> element;
-            if (set.contains(element))
-            {
-                cout << "Element is in the set.\n";
-            }
-            else
-            {
-                cout << "Element is not in the set.\n";
-            }
-            break;
-        case 4:
-            cout << "Size of the set: " << set.size() << endl;
-            break;
-        case 5:
-            cout << "Elements in the set: ";
-            for (int element : set)
-            {
-                cout << element << " ";
-            }
-            cout << endl;
-            break;
-        case 6:
-        {
-            Set intersectionSet;
-            cout << "Enter elements of the other set (0 to stop):\n";
-            while (true)
-            {
-                cin >> element;
-                if (element == 0)
-                {
-                    break;
-                }
-                intersectionSet.add(element);
-            }
-            Set result = set.intersection(intersectionSet);
-            cout << "Intersection of the sets: ";
-            for (int element : result)
-            {
-                cout << element << " ";
-            }
-            cout << endl;
-            break;
-        }
-        case 7:
-        {
-            Set unionSet;
-            cout << "Enter elements of the other set (0 to stop):\n";
-            while (true)
-            {
-                cin >> element;
-                if (element == 0)
-                {
-                    break;
-                }
-                unionSet.add(element);
-            }
-            Set result = set.unionSet(unionSet);
-            cout << "Union of the sets: ";
-            for (int element : result)
-            {
-                cout << element << " ";
-            }
-            cout << endl;
-            break;
-        }
-        case 8:
-        {
-            Set differenceSet;
-            cout << "Enter elements of the other set (0 to stop):\n";
-            while (true)
-            {
-                cin >> element;
-                if (element == 0)
-                {
-                    break;
-                }
-                differenceSet.add(element);
-            }
-            Set result = set.difference(differenceSet);
-            cout << "Difference of the sets: ";
-            for (int element : result)
-            {
-                cout << element << " ";
-            }
-            cout << endl;
-            break;
-        }
-        case 9:
-        {
-            Set otherSet;
-            cout << "Enter elements of the other set (0 to stop):\n";
-            while (true)
-            {
-                cin >> element;
-                if (element == 0)
-                {
-                    break;
-                }
-                otherSet.add(element);
-            }
-            if (set.subset(otherSet))
-            {
-                cout << "The set is a subset of the other set.\n";
-            }
-            else
-            {
-                cout << "The set is not a subset of the other set.\n";
-            }
-            break;
-        }
-        case 0:
-            return 0;
-        default:
-            cout << "Invalid choice. Please try again.\n";
-        }
-    }
+void Set::Create(){
+	int x;
+	cout<<"\n Enter number of elements: ";
+	cin>>x;
+	for(int i =0;i<x;i++){
+		cout<<"\nEnter Element: ";
+		cin>>a[n++];
+	}	
 }
+
+void Set::Display(){
+	for(int i =0;i<n;i++){
+		cout<<a[i]<<" ";
+	}
+}
+
+
+int Set::Search(int x){
+	for(int i =0;i<n;i++){
+		if(x==a[i]) return 1;
+	}
+	return 0;
+}
+
+
+void Set::Union(Set A,Set B){
+	int i,x;
+	for(i =0;i<A.n;i++){
+		a[n++]= A.a[i];
+	}
+	for(i =0;i<B.n;i++){
+		x = B.a[i];
+		if(Search(x)==0){
+		 a[n++]=x;
+		 }	 
+	}
+}
+
+void Set::Intersect(Set A, Set B){
+	int x;
+	int size;
+		if(A.n>B.n) size = A.n;
+		else size = B.n;
+		
+	for(int i =0; i< size;i++){
+	x=B.a[i];
+	if(A.Search(x)==1){
+		a[n++]=x;	
+		}
+	}
+}
+
+
+int main(){
+Set set;
+Set A,B,C;
+	int ch;
+
+	while(true){
+		cout<<"\n---Set Operations---\n 1.Add element \n 2.Display Set elements \n 3.Search in Set \n 4.Union of Two sets \n 5.Intersection \n 6.Exit \n\n Enter choice ";
+		cin>>ch;
+			switch(ch){
+
+				case 1: 
+						  set.Create(); break;
+
+				case 2: 
+						set.Display(); break;
+
+				case 3: 
+						cout<<"\n Enter key to be searched ";
+						int key;
+						cin>>key;
+
+						if(set.Search(key)==1) cout<<"\n Found";
+						else cout<<"\n Not found";
+
+						break;
+				
+				case 4: A.n = B.n =  C.n =0;
+						  cout<<"\n Enter elements in set A";
+						  A.Create();
+						  cout<<"\n Enter elements in set B";
+						  B.Create();
+						  C.Union(A,B);
+						  C.Display();
+						  break;
+				case 5: A.n = B.n =  C.n =0;
+						  cout<<"\n Enter elements in set A";
+						  A.Create();
+						  cout<<"\n Enter elements in set B";
+						  B.Create();
+						  C.Intersect(A,B);
+						  C.Display();
+						  break;
+						  
+						  
+				case 6: exit(0); break;
+				default: cout<<"\nInvalid choice!";
+				break;
+			}//switch case loop
+		}//while loop
+return 0;}
