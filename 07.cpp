5a98804c23da4d0ebd49fa41470b778d5b62bd34
adhi1911 <amk2754@gmail.com>
2024-05-01 19:03:17 +0530

    Prim MST Modified

--- 07.cpp
+++ 07.cpp
@@ -1,61 +1,59 @@
-#include <iostream>
-#include <vector>
-#include <climits>
+#include<iostream>
+#include<queue>
+#include<vector>
 
 using namespace std;
 
-int minKey(vector<int>& key, vector<bool>& mstSet) {
-    int min = INT_MAX, min_index;
+class Graph{
+    public:
+    vector<vector<int>> graph;
 
-    for (int v = 0; v < key.size(); v++)
-        if (mstSet[v] == false && key[v] < min)
-            min = key[v], min_index = v;
-
-    return min_index;  
-}
-
-void printMST(vector<int>& parent, vector<vector<int>>& graph) {
-    cout << "Edge \tWeight\n";
-    for (int i = 1; i < graph.size(); i++)
-        cout << parent[i] << " - " << i << " \t" << graph[i][parent[i]] << " \n";
-}
-
-void primMST(vector<vector<int>>& graph) {
-    vector<int> parent(graph.size());
-    vector<int> key(graph.size(), INT_MAX);
-    vector<bool> mstSet(graph.size(), false);
-
-    key[0] = 0;
-    parent[0] = -1;
-
-    for (int count = 0; count < graph.size() - 1; count++) {
-        int u = minKey(key, mstSet);
-
-        mstSet[u] = true;
-
-        for (int v = 0; v < graph.size(); v++)
-            if (graph[u][v] && mstSet[v] == false && graph[u][v] < key[v])
-                parent[v] = u, key[v] = graph[u][v];
-    }
-
-    printMST(parent, graph);
-}
-
-int main() {
-    int V;
-    cout << "Enter the number of offices: ";
-    cin >> V;
-
-    vector<vector<int>> graph(V, vector<int>(V, 0));
-
-    cout << "Enter the cost matrix:\n";
-    for(int i = 0; i < V; i++) {
-        for(int j = 0; j < V; j++) {
-            cin >> graph[i][j];
+    void CreateGraph(int V){
+        graph = vector<vector<int>>(V, vector<int>(V));
+        for(int i=0 ; i<V;i++){
+            for(int j =0; j<V;j++){
+                cin>>graph[i][j];
+            }
         }
     }
 
-    primMST(graph);
+    int primMst(int V){
+        int sum = 0;
+        priority_queue< pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+
+        vector<int> vis(V,0);
+        vector<int> parent(V,-1);
+        pq.push({0,0});
+        while(!pq.empty()){
+            auto it = pq.top();
+            pq.pop();
+            int node = it.second;
+            int wt = it.first;
+            if(vis[node]) continue;
+            vis[node] = true;
+            sum += wt;
+            for(int adjNode = 0; adjNode < V; adjNode++){
+            if(graph[node][adjNode] && !vis[adjNode]){
+            pq.push({graph[node][adjNode], adjNode});
+            parent[adjNode] = node;
+                }
+            }
+        }
+
+        for(int i =1; i<V;i++){
+            cout<<parent[i]<<" - "<<i<<endl;
+        }
+        return sum;
+    }
+};
+
+int main(){
+    Graph graph;
+    cout<<"\nEnter Number of vertices in graph: ";
+    int V;
+    cin>>V;
+    graph.CreateGraph(V);
+    cout<<graph.primMst(V);
 
     return 0;
 }
